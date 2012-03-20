6cf765107f5db8028f62eaa9b904f45e97ca1b54
Luke Braly <lbraly@wordrad.com>
2012-03-19 20:11:57 -0500

    convert unicode fixtures

---
+++ public/website/fixtures/u_esc_conv.rb
@@ -0,0 +1,16 @@
+#First convert unicode sequences in the file from \u0045 ri \u{0045}
+#Then run this to convert file from to take out those sequences and
+#replace them with utf=8 (or whatever it is)
+
+
+f = File.open(ARGV[0],"r")
+g = File.open(ARGV[1],"w")
+
+
+f.each_line do |s|
+  g.puts (eval "\"#{s}\"")
+end
+
+
+f.close
+g.close
--- test.rb
+++ test.rb
@@ -1,18 +1,11 @@
-module Mod
-  
-  class String
-    def dq
-      inspect 
-    end    
-  end
+f = File.open(ARGV[0],"r")
+g = File.open(ARGV[1],"w")
 
-  def f
-    puts "F"
-  end  
-  
-end  
-    
-  include Mod
-  
-  
-  puts "cccccccc".dq
+
+f.each_line do |s|
+  g.puts (eval "\"#{s}\"")
+end
+
+
+f.close
+g.close
